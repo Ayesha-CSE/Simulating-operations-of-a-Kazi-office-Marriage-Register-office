7a91167f7c89e7f7da326f72b88c372b0aaab308
Ayesha-IUb <2211155@iub.edu.bd>
2025-11-25 17:36:59 +0600

    Add CreateMarriageCase FXML and controller for marriage case management

---
+++ src/main/java/com/csc213/kazioffice_marriageregisteroffice/Kazi/CreateMarriageCaseController.java
@@ -0,0 +1,47 @@
+package com.csc213.kazioffice_marriageregisteroffice.Kazi;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class CreateMarriageCaseController
+{
+    @javafx.fxml.FXML
+    private TableColumn maritalColum;
+    @javafx.fxml.FXML
+    private TextField contactText;
+    @javafx.fxml.FXML
+    private TextField idnumberText;
+    @javafx.fxml.FXML
+    private TableColumn contactColum;
+    @javafx.fxml.FXML
+    private TableColumn ageColum;
+    @javafx.fxml.FXML
+    private TextField addressText;
+    @javafx.fxml.FXML
+    private TableView tableView;
+    @javafx.fxml.FXML
+    private TextField nameText;
+    @javafx.fxml.FXML
+    private TableColumn idNumberColum;
+    @javafx.fxml.FXML
+    private TableColumn nameColum1;
+    @javafx.fxml.FXML
+    private TableColumn nameColum;
+    @javafx.fxml.FXML
+    private ComboBox maritalCombox;
+    @javafx.fxml.FXML
+    private TableColumn addressColum;
+    @javafx.fxml.FXML
+    private TextField ageText;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void SaveButton(ActionEvent actionEvent) {
+    }
+}
