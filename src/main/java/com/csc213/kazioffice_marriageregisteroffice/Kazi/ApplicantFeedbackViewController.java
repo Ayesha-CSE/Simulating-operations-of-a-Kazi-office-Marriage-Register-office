1602319d2ae5ac98ba278a5926668261fa031106
Ayesha-IUb <2211155@iub.edu.bd>
2025-11-28 13:39:16 +0600

    Add FXML files and controllers for various marriage case functionalities

---
+++ src/main/java/com/csc213/kazioffice_marriageregisteroffice/Kazi/ApplicantFeedbackViewController.java
@@ -0,0 +1,54 @@
+package com.csc213.kazioffice_marriageregisteroffice.Kazi;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class ApplicantFeedbackViewController
+{
+    @javafx.fxml.FXML
+    private DatePicker filterDate;
+    @javafx.fxml.FXML
+    private TableColumn ratingColumn;
+    @javafx.fxml.FXML
+    private TableColumn feedbackColumn;
+    @javafx.fxml.FXML
+    private TableColumn applicantColumn;
+    @javafx.fxml.FXML
+    private TextField caseID;
+    @javafx.fxml.FXML
+    private ComboBox ratingField;
+    @javafx.fxml.FXML
+    private TableColumn caseIDColumn;
+    @javafx.fxml.FXML
+    private TableView tableView;
+    @javafx.fxml.FXML
+    private TextField applicantName;
+    @javafx.fxml.FXML
+    private TableColumn dateColumn;
+    @javafx.fxml.FXML
+    private TextArea feedbackArea;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void markAsReviewedButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void replyButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void applyFilter(ActionEvent actionEvent) {
+    }
+}
