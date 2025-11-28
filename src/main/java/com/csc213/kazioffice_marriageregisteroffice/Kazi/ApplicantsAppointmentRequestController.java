7a4fdb4771d1b5cfcb3d0de7eb5d27e8064243cb
Ayesha-IUb <2211155@iub.edu.bd>
2025-11-28 15:36:23 +0600

    Add FXML files and controllers for various marriage case functionalities

---
+++ src/main/java/com/csc213/kazioffice_marriageregisteroffice/Kazi/ApplicantsAppointmentRequestController.java
@@ -0,0 +1,49 @@
+package com.csc213.kazioffice_marriageregisteroffice.Kazi;
+
+public class ApplicantsAppointmentRequestController
+{
+    @javafx.fxml.FXML
+    private TableColumn applicantNameColumn;
+    @javafx.fxml.FXML
+    private TableColumn requesteddateColumn;
+    @javafx.fxml.FXML
+    private TextField requestedTime;
+    @javafx.fxml.FXML
+    private TableColumn requestedTimeColumn;
+    @javafx.fxml.FXML
+    private TableColumn satusColumn;
+    @javafx.fxml.FXML
+    private TextField caseID;
+    @javafx.fxml.FXML
+    private TextArea requedtDetails;
+    @javafx.fxml.FXML
+    private TableColumn caseIDColumn;
+    @javafx.fxml.FXML
+    private TableView tableView;
+    @javafx.fxml.FXML
+    private TextField applicantName;
+    @javafx.fxml.FXML
+    private DatePicker requestedDate;
+    @javafx.fxml.FXML
+    private TextField status;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void filterBtn(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backBtn(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void approveBtn(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void rejectBtn(ActionEvent actionEvent) {
+    }
+}
