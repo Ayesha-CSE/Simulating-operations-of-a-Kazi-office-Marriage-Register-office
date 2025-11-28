e507fea08c63efa184024f15fe2e2ce1190e633e
Ayesha-IUb <2211155@iub.edu.bd>
2025-11-28 12:58:00 +0600

    Add FXML files and controllers for various marriage case functionalities

---
+++ src/main/java/com/csc213/kazioffice_marriageregisteroffice/Kazi/CreateMarriageSchedualsController.java
@@ -0,0 +1,42 @@
+package com.csc213.kazioffice_marriageregisteroffice.Kazi;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class CreateMarriageSchedualsController
+{
+    @javafx.fxml.FXML
+    private ComboBox selectVenue;
+    @javafx.fxml.FXML
+    private TableColumn caseIdColumn;
+    @javafx.fxml.FXML
+    private TableColumn timeColumn;
+    @javafx.fxml.FXML
+    private ComboBox selectTime;
+    @javafx.fxml.FXML
+    private TextField caseId;
+    @javafx.fxml.FXML
+    private TableView tableView;
+    @javafx.fxml.FXML
+    private TableColumn venueColumn;
+    @javafx.fxml.FXML
+    private TableColumn dateColumn;
+    @javafx.fxml.FXML
+    private DatePicker selectDate;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void generateButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveButton(ActionEvent actionEvent) {
+    }
+}
