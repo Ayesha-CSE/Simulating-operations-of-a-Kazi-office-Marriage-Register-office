376e966155d264301cc312090693a04f1d1c0f1a
Ayesha-IUb <2211155@iub.edu.bd>
2025-11-28 16:03:53 +0600

    Add FXML files and controllers for various marriage case functionalities

---
+++ src/main/java/com/csc213/kazioffice_marriageregisteroffice/Kazi/CreateCaseSummaryController.java
@@ -0,0 +1,37 @@
+package com.csc213.kazioffice_marriageregisteroffice.Kazi;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class CreateCaseSummaryController
+{
+    @javafx.fxml.FXML
+    private TextField sessionField;
+    @javafx.fxml.FXML
+    private TextField caseId;
+    @javafx.fxml.FXML
+    private TextArea witnessNotes;
+    @javafx.fxml.FXML
+    private TextField marrigeCondition;
+    @javafx.fxml.FXML
+    private TextArea caseDetails;
+    @javafx.fxml.FXML
+    private TextField applicantName;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void generateSummaryBtn(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backBtn(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveBtn(ActionEvent actionEvent) {
+    }
+}
